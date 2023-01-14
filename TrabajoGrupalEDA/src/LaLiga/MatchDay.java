d814c04a75e7d25845c9270a3ae14875d18f3142
Antonio-Gabarrus <gabatono@gmail.com>
2023-01-14 11:09:56 +0100

    AGN - PrioQueue for Matches in MatchDay

--- TrabajoGrupalEDA/src/LaLiga/MatchDay.java
+++ TrabajoGrupalEDA/src/LaLiga/MatchDay.java
@@ -7,21 +7,23 @@ public class MatchDay {
 	final String[] WEEK_DAYS = {"Friday", "Saturday", "Sunday", "Thursday"};
 	int leg;
 	int matchDayNumber;
-	SingleLinkedList matches;
+	PriorityQueue matches;
 	Team restingTeam;
+	SingleLinkedList matchesPlayed;
 	
 	public MatchDay(int leg, int matchDayNumber) {
 		this.leg = leg;
 		this.matchDayNumber = matchDayNumber;
-		this.matches = new SingleLinkedList();
+		this.matches = new PriorityQueue(MatchDay.WEEK_DAYS.length);
 		this.restingTeam = null;
+		this.matchesPlayed = new SingleLinkedList();
 	}
 	
 	public boolean addMatch(Match match) {
 		boolean success = !this.isTeamPlaying(match.getHomeTeam()) && !this.isTeamPlaying(match.getAwayTeam()) 
 		&& match.getHomeTeam() != this.restingTeam && match.getAwayTeam() != this.restingTeam;
 		if(success) {
-			matches.insertHead(match);
+			matches.push(match,setDayOfMatch(match));
 		}
 		return success;
 	}
@@ -43,27 +45,40 @@ public class MatchDay {
 	}
 	
 	public boolean isTeamPlaying(Team team) {
-		return team.isActive();
-	}
+        boolean isPlaying = false;
+        PriorityQueue aux = new PriorityQueue(MatchDay.WEEK_DAYS.length);
+        while (!this.matches.isEmpty()) {
+            Match match = (Match) matches.poll();
+            isPlaying = isPlaying || match.getAwayTeam() == team || match.getHomeTeam() == team;
+            int prio = (match.getDay().equals(MatchDay.WEEK_DAYS[0])) ? 1 : 
+                       (match.getDay().equals(MatchDay.WEEK_DAYS[1])) ? 2 : 
+                       (match.getDay().equals(MatchDay.WEEK_DAYS[2])) ? 3 : 0;
+            aux.push(match, prio);
+        }
+        this.matches = aux;
+        return isPlaying;
 	
 	public void playMatchDay() {
 		for (int i = 0; i < this.matches.size; i++){
-			((Match)(this.matches.get(i).data)).simulate();
+			Match playingMatch = (Match)(this.matches.poll()); 
+			playingMatch.simulate();
+			matchesPlayed.insertLast(playingMatch);
 			//System.out.println(((Match)(this.matches.get(i).data)));
 		}
 	}
 	
-	private void setDayOfMatch(Match match) {
-		if(match.getHomeTeam().hasPlayedOnSunday() || match.getAwayTeam().hasPlayedOnSunday()){
-			match.setDay(WEEK_DAYS[(int) Math.rand()*3]);
-		} else {
-			match.setDay(WEEK_DAYS[(int) Math.rand()*4]);
-		}
-		if(match.getDay().equals(WEEK_DAYS[2])){
-			match.getAwayTeam().setHasPlayedOnSunday(true);
-			match.getHomeTeam().setHasPlayedOnSunday(true);
-		}
-	}
+	private int setDayOfMatch(Match match) {
+        int day = (match.getAwayTeam().hasPlayedOnSunday() || match.getHomeTeam().hasPlayedOnSunday()) ? 
+                    (int)(Math.random()*3) : 
+                    (int)(Math.random()*4);
+        match.setDay(Matchday.WEEK_DAYS[day]);
+        if (match.getDay().equals(Matchday.WEEK_DAYS[2])){
+            match.getAwayTeam().setHasPlayedOnSunday(true);
+            match.getHomeTeam().setHasPlayedOnSunday(true);
+        }
+        
+        return (day + 1) % 4;
+    }
 	
 	public void setRestingTeam(Team restingTeam) {
 		this.restingTeam = restingTeam;
