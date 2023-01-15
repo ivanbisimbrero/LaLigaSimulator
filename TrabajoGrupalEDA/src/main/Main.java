07a4e3a5377d53316fa9f58038b6484cac7d0c46
KyloRenCodes <88286610+Codemaster09@users.noreply.github.com>
2023-01-15 12:24:39 +0100

    Main tests

--- TrabajoGrupalEDA/src/main/Main.java
+++ TrabajoGrupalEDA/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import LaLiga.League;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		League LaLigaSantander = new League("La Liga Santander");
+		LaLigaSantander.simulateSession();
 	}
 
 }
